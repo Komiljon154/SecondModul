f20102b985798d2ee3b5a3dc4f16013f857b3752
Komiljon154 <komiljonk154@gmail.com>
2024-12-12 08:40:28 +0500

    Proj created

---
+++ 2.5dars/2.5dars.Api/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2._5dars.Api
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
